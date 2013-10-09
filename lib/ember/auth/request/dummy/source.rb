heartsentwined/ember-auth-response-json

ruby source gem api

lib/ember/auth/response/json/source.rb
@@ -1,0 +1,15 @@
+module Ember
+  module Auth
+    module Response
+      module Json
+        module Source
+          def self.bundled_path
+            path = File.join '..', '..', '..', '..', '..', '..', \
+              'dist', 'ember-auth-response-json.js'
+            File.expand_path path, __FILE__
+          end
+        end
+      end
+    end
+  end
+end

heartsentwined/ember-auth-request-dummy

lib/ember/auth/request/dummy/source.rb
@@ -1,0 +1,15 @@
+module Ember
+  module Auth
+    module Request
+      module Dummy
+        module Source
+          def self.bundled_path
+            path = File.join '..', '..', '..', '..', '..', '..', \
+              'dist', 'ember-auth-request-dummy.js'
+            File.expand_path path, __FILE__
+          end
+        end
+      end
+    end
+  end
+end